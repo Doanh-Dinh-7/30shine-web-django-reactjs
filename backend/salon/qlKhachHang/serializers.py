9de86f5f37929b9f798c5fab7b8559f72833b8d6
tuhuong7 <lehuongg1876@gmail.com>
2025-05-15 14:50:43 +0700

    Sửa: Correct UserSerializer class name and add package-lock.json

--- backend/salon/qlKhachHang/serializers.py
+++ backend/salon/qlKhachHang/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import KhachHang
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializerse(rializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
