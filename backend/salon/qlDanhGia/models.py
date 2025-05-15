c16f2c983474797eccb08cb3fdd7d22442fb3c4f
tuhuong7 <lehuongg1876@gmail.com>
2025-05-16 03:34:25 +0700

    Sửa file Model và Serializers

--- backend/salon/qlDanhGia/models.py
+++ backend/salon/qlDanhGia/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from qlKhachHang.models import KhachHang
+from qlDichVu.models import DichVu
 
 class DanhGia(models.Model):
     MaDG = models.AutoField(primary_key=True)
     MaKH = models.ForeignKey(KhachHang, on_delete=models.CASCADE, related_name='danh_gia')
-    NgayDanhGia = models.DateField(auto_now_add=True)
-    NoiDung = models.TextField()
+    MaDV = models.ForeignKey(DichVu, on_delete=models.CASCADE, related_name='danh_gia')
     DiemDanhGia = models.IntegerField()
+    NoiDung = models.TextField()
+    NgayDanhGia = models.DateField(auto_now_add=True)
+    
 
     def __str__(self):
         return f"Đánh giá {self.MaDG} - {self.MaKH.HoTenKH}"
--- backend/salon/qlDanhGia/serializers.py
+++ backend/salon/qlDanhGia/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import DanhGia
 
 class DanhGiaSerializer(serializers.ModelSerializer):
+    TenKH = serializers.CharField(source='MaKH.HoTenKH', read_only=True)
+    TenDV = serializers.CharField(source='MaDV.TenDV', read_only=True)
+    
     class Meta:
         model = DanhGia
-        fields = ['MaDG', 'MaKH', 'NgayDanhGia', 'NoiDung', 'DiemDanhGia'] 
+        fields = ['MaDG', 'MaKH', 'MaDV', 'TenDV', 'TenKH', 'DiemDanhGia', 'NoiDung', 'NgayDanhGia']
+        # fields = ['MaDG', 'TenDV', 'TenKH','DiemDanhGia',  'NoiDung', 'NgayDanhGia'] 
