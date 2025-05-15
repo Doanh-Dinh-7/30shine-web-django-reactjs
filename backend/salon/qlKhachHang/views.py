15b41ccbf0d4d49e0c8810a061b29f4da7b69ad2
tuhuong7 <lehuongg1876@gmail.com>
2025-05-15 15:15:40 +0700

    feat: Thêm thông báo khi xoá khách hàng thành công và không thành công

--- backend/salon/qlKhachHang/views.py
+++ backend/salon/qlKhachHang/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import KhachHang
 from .serializers import KhachHangSerializer
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
 
 class KhachHangViewSet(viewsets.ModelViewSet):
     queryset = KhachHang.objects.all()
     serializer_class = KhachHangSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except NotFound:
+            return Response(
+                {"message": "Không tìm thấy khách hàng để xoá."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Thông tin khách hàng đã bị xoá."},
+            status=status.HTTP_200_OK
+        )
