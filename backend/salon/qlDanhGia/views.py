30f448bd04e47751388ca9b68b7ae3aebc824a30
tuhuong7 <lehuongg1876@gmail.com>
2025-05-15 15:37:28 +0700

    thêm thông báo khi xoá đánh giá

--- backend/salon/qlDanhGia/views.py
+++ backend/salon/qlDanhGia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import DanhGia
 from .serializers import DanhGiaSerializer
 
@@ -8,3 +9,11 @@ from .serializers import DanhGiaSerializer
 class DanhGiaViewSet(viewsets.ModelViewSet):
     queryset = DanhGia.objects.all()
     serializer_class = DanhGiaSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Đánh giá đã được xoá thành công."},
+            status=status.HTTP_200_OK
+        )
