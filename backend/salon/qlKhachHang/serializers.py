6712f4c4aa0c19df360d7866f18603f4ac9d839f
tuhuong7 <lehuongg1876@gmail.com>
2025-05-15 14:56:17 +0700

    Sửa lỗi class

--- backend/salon/qlKhachHang/serializers.py
+++ backend/salon/qlKhachHang/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import KhachHang
 from django.contrib.auth.models import User
 
-class UserSerializerse(rializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
